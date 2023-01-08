35385f904ddfdd0fd5678d7961ee6c245d000a87
mohandeep.amd <mohandeep.amd@holosuit.com>
2023-01-08 23:11:20 +0530

    crispy forms added

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from .forms import EmployeeForm
 # Create your views here.
 
 def employee_list(request):
     return render(request, "employee_register/employee_list.html")
 
 def employee_form(request):
-    return render(request, "employee_register/employee_form.html")
+    form = EmployeeForm()
+
+    return render(request, "employee_register/employee_form.html", {'form':form})
 
 def employee_delete(request):
     return render(request, "employee_register/base.html")
